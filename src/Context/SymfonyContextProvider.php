8516dd768b13220b19add65238810c8dee0ef809
Dominik Chrástecký <dominik@chrastecky.cz>
2021-08-25 17:03:06 +0200

    Add new features from upstream (#2)

---
+++ src/Context/SymfonyContextProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Unleash\Client\Bundle\Context;
+
+use Unleash\Client\Configuration\Context;
+use Unleash\Client\ContextProvider\UnleashContextProvider;
+
+final class SymfonyContextProvider implements UnleashContextProvider
+{
+    public function __construct(
+        private SymfonyUnleashContext $context
+    ) {
+    }
+
+    public function getContext(): Context
+    {
+        return $this->context;
+    }
+}
