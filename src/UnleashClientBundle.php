0d40bac8e6f622858a15c5249b9c870e0b3bbf32
Dominik Chrastecky <dominik@chrastecky.cz>
2024-01-17 21:07:53 +0100

    Rename extension to expected format

--- src/UnleashClientBundle.php
+++ src/UnleashClientBundle.php
@@ -11,6 +11,9 @@ use Unleash\Client\Bundle\DependencyInjection\Compiler\CacheServiceResolverCompi
 use Unleash\Client\Bundle\DependencyInjection\Compiler\HttpServicesResolverCompilerPass;
 use Unleash\Client\Strategy\StrategyHandler;
 
+/**
+ * @internal
+ */
 final class UnleashClientBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
