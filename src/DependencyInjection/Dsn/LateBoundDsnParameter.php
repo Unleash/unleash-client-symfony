9d646629b9074b31798ecb586368f690792d4d42
Rikudou_Sage <dominik@chrastecky.cz>
2024-04-17 15:53:23 +0200

    Feat: Add DSN support for environment variables (#68)

---
+++ src/DependencyInjection/Dsn/LateBoundDsnParameter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Unleash\Client\Bundle\DependencyInjection\Dsn;
+
+use Stringable;
+
+final readonly class LateBoundDsnParameter implements Stringable
+{
+    public function __construct(
+        private string $envName,
+        private string $parameter,
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        $dsn = getenv($this->envName) ?: $_ENV[$this->envName] ?? null;
+        if ($dsn === null) {
+            return '';
+        }
+
+        $query = parse_url($dsn, PHP_URL_QUERY);
+        assert(is_string($query));
+        $instanceUrl = str_replace("?{$query}", '', $dsn);
+        if (str_contains($instanceUrl, '%3F')) {
+            $instanceUrl = urldecode($instanceUrl);
+        }
+        if ($this->parameter === 'url') {
+            return $instanceUrl;
+        }
+        parse_str($query, $queryParts);
+
+        $result = $queryParts[$this->parameter] ?? '';
+        assert(is_string($result));
+
+        return $result;
+    }
+}
